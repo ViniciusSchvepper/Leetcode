1032d54566543b0cc4d2ea0ee540357aa1844f29
ViniciusSchvepper <vynycyus_hecate@hotmail.com>
2024-06-11 12:28:22 -0300

    Adição de arquivo

---
+++ 1122_RelativeSortArray.py
@@ -0,0 +1,33 @@
+"""
+Given two arrays arr1 and arr2, the elements of arr2 are distinct, and all elements in arr2 are also in arr1.
+
+Sort the elements of arr1 such that the relative ordering of items in arr1 are the same as in arr2. Elements that do not appear in arr2 should be placed at the end of arr1 in ascending order.
+
+ 
+
+Example 1:
+
+Input: arr1 = [2,3,1,3,2,4,6,7,9,2,19], arr2 = [2,1,4,3,9,6]
+Output: [2,2,2,1,4,3,3,9,6,7,19]
+Example 2:
+
+Input: arr1 = [28,6,22,8,44,17], arr2 = [22,28,8,6]
+Output: [22,28,8,6,17,44]
+"""
+
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        sortedArray = []
+        notInArr2 = []
+
+        for i in range(len(arr2)):
+            for j in range(len(arr1)):
+                if arr2[i] == arr1[j]:
+                    sortedArray.append(arr1[j])
+        for i in range(len(arr1)):
+            if arr1[i] not in arr2:
+                notInArr2.append(arr1[i])
+
+        notInArr2.sort()
+        sortedArray = sortedArray + notInArr2
+        return sortedArray
